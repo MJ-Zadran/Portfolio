23feff0e37a118bce8ecd56a88e2576dbe853488
MJ-Zadran <125396089+MJ-Zadran@users.noreply.github.com>
2024-09-11 22:24:52 -0700

    Add files via upload
    Portfolio about my self.

---
+++ index.js
@@ -0,0 +1,8 @@
+//  typing animations
+
+var typed = new Typed(".typing",{
+    strings: ["","Web Designer", "web Developer", "JavaScript Developer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
